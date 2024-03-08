517456d1345a0bf1746a74889b471288ad93c9cb
chimekinglsey <onlinekingsley@gmail.com>
2024-03-08 21:51:25 +0100

    Serving staticfiles with whitenoise and media with s3

--- workforcehub/settings.py
+++ workforcehub/settings.py
@@ -179,20 +179,16 @@ if not DEBUG:
         "default": {
             "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
 
-        },
-
-        "staticfiles": {
-            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-        },
+        }
     } 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
 
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
     STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
     # Additional directories where Django should look for static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
